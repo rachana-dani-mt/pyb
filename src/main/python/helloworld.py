2c1d33e5a24a8e89ff668a0ad7ee95c15aa2344b
rdani <Rachana_SudhindraDani@intuit.com>
2024-03-08 10:31:45 +0530

    add module docstring

--- src/main/python/helloworld.py
+++ src/main/python/helloworld.py
@@ -1,8 +1,14 @@
 import sys
 
-'''
- A simple module to print 'Hello world' 
-'''
+"""
+This module contains a function named "helloworld" that 
+writes a message to the standard output stream. 
+The message is "Hello world from python project! :)". 
+The function takes an argument "out" which is a file-like 
+object representing the output stream. 
+The function uses the "write" method of the "out" 
+object to write the message to the output stream.
+"""
 
 def helloworld(out):
     """ Uses sys out to write message """
