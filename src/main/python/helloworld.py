34f238d25de2fe1a93a8a1e248a3b3e4ca112132
rdani <Rachana_SudhindraDani@intuit.com>
2024-03-08 10:26:47 +0530

    address failures

--- src/main/python/helloworld.py
+++ src/main/python/helloworld.py
@@ -1,11 +1,9 @@
 import sys
 
-""" Prints message """
+""" A simple module to print 'Hello world' """
 
 def helloworld(out):
-
     """ Uses sys out to write message """
-
     out.write("Hello world from python project! :)\n")
 
 helloworld(sys.stdout)
