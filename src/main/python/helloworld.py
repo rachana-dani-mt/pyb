e3b4d232a7fa647f8071bccdc3092b5a26c3a4c3
rdani <Rachana_SudhindraDani@intuit.com>
2024-03-08 10:32:50 +0530

    add module docstring

--- src/main/python/helloworld.py
+++ src/main/python/helloworld.py
@@ -1,17 +1,17 @@
+"""
+This is a simple module to demonstrate a hello world function in Python.
+"""
+
 import sys
 
-"""
-This module contains a function named "helloworld" that 
-writes a message to the standard output stream. 
-The message is "Hello world from python project! :)". 
-The function takes an argument "out" which is a file-like 
-object representing the output stream. 
-The function uses the "write" method of the "out" 
-object to write the message to the output stream.
-"""
-
 def helloworld(out):
-    """ Uses sys out to write message """
-    out.write("Hello world from python project! :)\n")
+    """
+    Print a hello world message.
 
+    Args:
+        out: Output stream to write the message to.
+    """
+    out.write("Hello world of Python\n")
+
+# Call the function with a file object
 helloworld(sys.stdout)
--- src/unittest/python/helloworld_tests.py
+++ src/unittest/python/helloworld_tests.py
@@ -7,4 +7,4 @@ class HelloWorldTest(unittest.TestCase):
     def test_should_issue_hello_world_message(self):
         out = mock()
         helloworld(out)
-        verify(out).write("Hello world from python project! :)\n")
+        verify(out).write("Hello world of Python\n")
